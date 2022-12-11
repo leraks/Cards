07f4831bcbce2a4bbcdc8d679085bac4c6015ce8
Ilya <67760549+leraks@users.noreply.github.com>
2022-12-11 03:26:23 -0800

    Delete cards directory

--- cards/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class PurchaseAdmin(admin.ModelAdmin):
-    readonly_fields = ('statuc',)
-
-admin.site.register(Card)
-admin.site.register(Purchase, PurchaseAdmin)
--- cards/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', search, name="search"),
-    path('cards/<int:pk>/', views_cards, name="views_cards"),
-    path('create/', create_card, name="create_card"),
-
-]
